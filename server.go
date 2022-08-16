4857aa943a366265d4d774dae548f79dd2e0ea06
horvski <horvski@gmail.com>
2022-08-16 10:55:28 -0600

    local server development

---
+++ server.go
@@ -0,0 +1,30 @@
+// local dev server
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	log.Println("Listening on :8082...")
+	http.HandleFunc("/", serveFiles) // "/" matches everything (See ServeMux)
+	log.Fatal(http.ListenAndServeTLS(":8082", "server.crt", "server.key", nil))
+}
+
+func serveFiles(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Request: %s\n", r.URL.Path)
+
+	var filePath = r.URL.Path[1:] //remove slash
+	if filePath == "" {
+		// On empty path display home/index (`test.html`)
+		filePath = "index.html"
+	} else if filePath == "test_run.js" || filePath == "test.js" || filePath == "example.js" || filePath == "urlform.js" {
+		// Do nothing, serve filepath unmodified.
+	} else {
+		filePath = "../" + filePath
+	}
+
+	log.Printf("Serving: %s", filePath)
+	http.ServeFile(w, r, filePath)
+}
